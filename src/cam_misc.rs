7abee90c5a003c1411ef4793820fd900397886be
David O'Connor <the_alchemist@fastmail.com>
2025-10-02 08:45:31 -0400

    sync

--- src/cam_misc.rs
+++ src/cam_misc.rs
@@ -18,7 +18,7 @@ use crate::{
 
 const MOVE_TO_TARGET_DIST: f32 = 15.;
 pub const MOVE_CAM_TO_LIG_DIST: f32 = 30.;
-const MOVE_TO_CAM_DIST: f32 = 25.;
+const MOVE_TO_CAM_DIST: f32 = 20.;
 
 /// Move the camera to look at a point of interest. Takes the starting location into account.
 /// todo: Smooth interpolated zoom.
