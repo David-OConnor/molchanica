b46cb2a0229fcf263defc82b5f0495d227bff817
David O'Connor <the_alchemist@fastmail.com>
2026-07-19 08:34:28 -0400

    sync

--- make_release.ps1
+++ make_release.ps1
@@ -6,13 +6,14 @@ $exe    = Join-Path $outDir "molchanica.exe"
 $gemmi  = "C:\Program Files\gemmi"
 $readme = "README.md"
 $cufft = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.0\bin\x64\cufft64_12.dll"
+$opendde = "install_scripts/install_opendde_venv.ps1"
 
 cargo build --release
 $zip1 = "molchanica_${version}_win.zip"
 if (Test-Path $zip1) { Remove-Item $zip1 -Force }
-Compress-Archive -LiteralPath $exe, $gemmi, $readme, $cufft -DestinationPath $zip1 -Force
+Compress-Archive -LiteralPath $exe, $gemmi, $readme, $opendde, $cufft  -DestinationPath $zip1 -Force
 
 cargo build --release --no-default-features
 $zip2 = "molchanica_${version}_win_nocuda.zip"
 if (Test-Path $zip2) { Remove-Item $zip2 -Force }
-Compress-Archive -LiteralPath $exe, $gemmi, $readme -DestinationPath $zip2 -Force
+Compress-Archive -LiteralPath $exe, $gemmi, $readme, $opendde -DestinationPath $zip2 -Force
--- make_release.sh
+++ make_release.sh
@@ -8,13 +8,14 @@ readme="README.md"
 setup="install_scripts/setup_linux_desktop.sh"
 icon="resources/icon.png"
 cufft="/usr/local/cuda/lib64/libcufft.so.12"
+opendde="install_scripts/install_opendde_venv.sh"
 
 export PATH=/usr/local/cuda/bin:$PATH
 
 chmod +x "$setup"
 
 cargo build --release
-zip -j -r "molchanica_${version}_linux.zip" "$exe" "$readme" "$setup" "$icon" "$cufft"
+zip -j -r "molchanica_${version}_linux.zip" "$exe" "$readme" "$setup" "$icon" "$opendde" "$cufft"
 
 cargo build --release --no-default-features
-zip -j -r "molchanica_${version}_linux_nocuda.zip" "$exe" "$readme" "$setup" "$icon"
+zip -j -r "molchanica_${version}_linux_nocuda.zip" "$exe" "$readme" "$setup" "$icon" "$opendde"
